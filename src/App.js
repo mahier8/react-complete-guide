3978a2c0c81866d87bd452020a521fe8fd2b83f1
Mahier Sydow <mahier.sydow8@gmail.com>
2021-03-15 16:09:33 +0900

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working</p>
+        <Person name="Mahier" age="34" />
+        <Person name="Angel" age="28">My hobbies: playing striker</Person>
+        <Person name="Bobby" age="29" />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now'));
+  }
+}
+
+export default App;
