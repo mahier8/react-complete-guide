836de1b0751b37d6a8774890a013304559810ac3
Mahier Sydow <mahier.sydow8@gmail.com>
2021-03-16 18:58:51 +0900

    React hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = props => {
+  const [ personsState, setPersonsState] = useState({
+    persons: [
+      {name: "Mahier", age: 34},
+      {name: "Angel", age: 28},
+      {name: "Bobby", age: 29}
+    ], 
+  });
+
+  const [ otherState, setOtherState] = useState("some other value");
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    // console.log("was clicked");
+    // Dont do this: this.state.persons[0].name = "Mahier Sydow";
+    setPersonsState({    
+      persons: [
+      {name: "Mahier Sydow", age: 34},
+      {name: "Angel", age: 28},
+      {name: "Bobby", age: 30}
+    ],
+  });
+  };
+  
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person name="Mahier" age="34" />
-        <Person name="Angel" age="28">My hobbies: playing striker</Person>
-        <Person name="Bobby" age="29" />
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person 
+        name={personsState.persons[0].name} 
+        age={personsState.persons[0].age} />
+        <Person 
+        name={personsState.persons[1].name} 
+        age={personsState.persons[1].age}>My hobbies: playing striker</Person>
+        <Person 
+        name={personsState.persons[2].name} a
+        ge={personsState.persons[2].age} />personsState
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now'));
-  }
-}
+    {/* // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now')); */}
+};
 
-export default App;
+export default app;
